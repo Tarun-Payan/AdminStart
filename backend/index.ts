dd3c467b6b8847514f2d04e7cfae32d0a8ecebe0
Tarun-Payan <tarunpayan8@gmail.com>
2026-05-11 07:50:34 +0530

    setup drizzle orm with postgresql

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
 const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+const db = drizzle({ 
+  connection: { 
+    connectionString: process.env.DATABASE_URL!,
+    ssl: true
+  }
+});
 
 app.use(express.static('public'))
 
